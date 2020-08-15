6a551b415f9d9240ac481e3dacc263095fedc959
Parv <parvkamal@gmail.com>
2020-08-15 17:09:31 +0530

    CSS Module Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
-import './App1.css';
+import './App.css';
+// import './App1.css';
 import BlogCard from './BlogCard';
 import { isArrayEmpty } from './Utils';
 
--- src/BlogCard.js
+++ src/BlogCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { dumpLogs } from './Utils';
+import classes from './BlogCard.module.css';
+// import './BlogCard.css'
 
 
 const BlogCard = (props) => {
     dumpLogs(props);
     return (
-        <div className="box1style">
+        <div className={classes.box1style}>
             <h3> {props.title}</h3>
             <p> {props.description} </p>
         </div>
