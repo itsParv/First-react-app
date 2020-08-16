1d15b180a1df8d87d2bd8bdbfa4d6579eb2d76f1
Parv <parvkamal@gmail.com>
2020-08-16 11:55:32 +0530

    Like functionality added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import './App1.css';
 import BlogCard from './BlogCard';
@@ -8,43 +7,45 @@ import { isArrayEmpty } from './Utils';
 
 class App extends React.Component {
   state = {
-    showBlogs: true
+    showBlogs: true,
+
+    blogArr: [
+      {
+        id: 1,
+        title: 'Blog Title 1',
+        description: 'Lorem Ipsum Dolo Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+        likeCount: 0
+      },
+      {
+        id: 2,
+        title: 'Blog Title 2',
+        description: 'Lorem Ipsum Dolo Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+        likeCount: 0
+      },
+      {
+        id: 3,
+        title: 'Blog Title 3',
+        description: 'Lorem Ipsum Dolo Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+        likeCount: 0
+      }
+    ]
   }
 
 
-  blogArr = [
-    {
-      id: 1,
-      title: 'Blog Title 1',
-      description: 'Lorem Ipsum Dolo Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-    },
-    {
-      id: 2,
-      title: 'Blog Title 2',
-      description: 'Lorem Ipsum Dolo Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-    },
-    {
-      id: 3,
-      title: 'Blog Title 3',
-      description: 'Lorem Ipsum Dolo Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-    }
-  ]
+
+  onLikeButtonClick = (pos) => {
+    const updatedBlogList = this.state.blogArr;
+    const updatedBlogObj = updatedBlogList[pos];
+    // console.log(updatedBlogObj);
+    updatedBlogObj.likeCount = updatedBlogObj.likeCount + 1;
+    updatedBlogList[pos] = updatedBlogObj;
+
+    this.setState({ blogArr: updatedBlogList })
+  }
+
+  // const blogArr = null; 
 
 
-  // const blogArr = null;
-
-  blogCards = isArrayEmpty(this.blogArr) ? [] : this.blogArr.map((item, pos) => {
-    // console.log(item);
-
-    return (
-      <BlogCard key={pos} title={item.title} description={item.description} id={item.id} />
-      // <div className="box1style" key={item.id} >
-      //   <h3>{item.title} </h3>
-      //   <p> {item.description} </p>
-      // </div>
-    )
-
-  })
 
   onHideButtonCLick = () => {
     // let updatedState = !this.state.showBlogs
@@ -56,12 +57,31 @@ class App extends React.Component {
 
   }
 
+
+
   render() {
+    console.log("render called");
+    console.log(this.state);
+    
+    const blogCards = isArrayEmpty(this.state.blogArr) ? [] : this.state.blogArr.map((item, pos) => {
+      // console.log(item);
+  
+      return (
+        <BlogCard key={pos} title={item.title}
+          description={item.description} id={item.id} likeCount={item.likeCount}
+          onLikeButtonClick={() => this.onLikeButtonClick(pos)} />
+        // <div className="box1style" key={item.id} >
+        //   <h3>{item.title} </h3>
+        //   <p> {item.description} </p>
+        // </div>
+      )
+  
+    })
     return (
       <div className="App" >
         <button onClick={this.onHideButtonCLick}> {this.state.showBlogs ? 'Hide Blog' : 'Show Blog'}  </button>
         <br></br>
-        {this.state.showBlogs ? this.blogCards : null}
+        {this.state.showBlogs ? blogCards : null}
 
       </div>
     );
--- src/BlogCard.js
+++ src/BlogCard.js
@@ -5,13 +5,26 @@ import classes from './BlogCard.module.css';
 
 
 const BlogCard = (props) => {
+
+    // onLikeBtnClick = () => {
+    //     this.setState((prevState, prevProp) => {
+    //         return {likeCount : prevState.likeCount+1}})
+    // }
+
+
     dumpLogs(props);
     return (
         <div className={classes.box1style}>
             <h3> {props.title}</h3>
             <p> {props.description} </p>
+
+            <p>Like Count:
+                 <span className={classes.likeCount}> {props.likeCount} </span>
+            </p>
+            <button onClick={props.onLikeButtonClick}> Like </button>
         </div>
     )
+
 }
 
 export default BlogCard;
