a2f08af33e1658a078d6e96237e30f173f5e48ec
Parv <parvkamal@gmail.com>
2020-08-16 11:58:52 +0530

    build added

---
+++ build/precache-manifest.6ab499223b5acdb5a2fdcebf1bcd8c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b62d2b4e73d37ddcbbc61b7d9b0a248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c507175b1879fcff471",
+    "url": "/static/css/main.2c7d7b0c.chunk.css"
+  },
+  {
+    "revision": "16017adc39f1241568f1",
+    "url": "/static/js/2.0a7bcca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a7bcca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c507175b1879fcff471",
+    "url": "/static/js/main.b1235405.chunk.js"
+  },
+  {
+    "revision": "450a43cc227ab816b7bc",
+    "url": "/static/js/runtime-main.b62c9b67.js"
+  }
+]);
